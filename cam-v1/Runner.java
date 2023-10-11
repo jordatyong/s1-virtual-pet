fd91067d49f9c2415368c6fa37e7cbd306b6beaf
Sean Morris <smorris@ausdk12.org>
2023-10-11 11:31:47 -0700

    w9 start

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,8 +1,37 @@
+import javax.swing.*;
+
 public class Runner{
-    public static void main(String[] args) {
+    public Runner(){
         VirtualPet v = new VirtualPet();
         v.feed();
-        v.exercise();  
-        v.sleep();      
+        takeABeat(1000);
+        v.exercise();
+        String ans = getAnswer("How are you?");
+        if(ans.equalsIgnoreCase("Good")){
+
+        }
+    }
+    
+    public String getAnswer(String q){
+        String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    q,
+                    "Get a response",
+                    JOptionPane.PLAIN_MESSAGE
+        );
+        if (s.equals("Yes")){
+            return "Yes";        
+        }
+    }
+    public void takeABeat(int milliseconds){
+        try {
+            Thread.sleep(1000); //milliseconds
+        } catch(Exception e){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new Runner();
     }
 }
